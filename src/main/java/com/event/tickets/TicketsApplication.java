183b2e07de7635e979a67277cde7986fbf77c1ee
learning_git <shivamittal616@gmail.com>
2025-10-12 19:35:23 +0530

    added swagger

--- src/main/java/com/event/tickets/TicketsApplication.java
+++ src/main/java/com/event/tickets/TicketsApplication.java
@@ -1,9 +1,16 @@
 package com.event.tickets;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Event Ticket Platform API",
+		version = "1.0",
+		description = "API for managing events and tickets"
+))
 public class TicketsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/event/tickets/config/SecurityConfig.java
+++ src/main/java/com/event/tickets/config/SecurityConfig.java
@@ -3,15 +3,25 @@ package com.event.tickets.config;
 
 
 import com.event.tickets.filters.UserProvisioningFilter;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.Converter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig {
@@ -24,6 +34,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize ->
                         authorize
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/swagger-ui.html").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
                                 .requestMatchers("/api/v1/events").hasRole("ORGANIZER")
                                 // Catch all rule
@@ -39,5 +51,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 
 }
